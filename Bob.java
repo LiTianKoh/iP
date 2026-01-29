6e945735cbf882726fc6234092ad5b551821b06d
Koh Li Tian <e1398429@u.nus.edu>
2026-01-29 14:42:29 +0800

    init commit

--- Bob.java
+++ Bob.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class Bob {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String line;
+
         String logo =
                 " ______   _______   ______ \n"
                         + "|  __  \\ |  ___  | |  __  \\ \n"
@@ -8,10 +15,35 @@ public class Bob {
                         + "| |__)  )| |___| | | |__)  )\n"
                         + "|______/ |_______| |______/ \n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("___________________________");
-        System.out.println("Hello! I'm BOB\n" + "What can I do for you?");
-        System.out.println("___________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("___________________________");
+        System.out.println("    ___________________________");
+        System.out.println("    Hello! I'm BOB");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ___________________________");
+
+        //Word recognition
+        Pattern pattern = Pattern.compile("handsome|beautiful", Pattern.CASE_INSENSITIVE);
+
+        do {
+            line = in.nextLine();
+            System.out.println("    ___________________________");
+
+            //Check if it's not Bye
+            if (!line.equalsIgnoreCase("Bye")) {
+                Matcher matcher = pattern.matcher(line);
+
+                //Check if the line input consist of handsome or beautiful
+                if (matcher.find()) {
+                    System.out.println("    Nonono, you are ;)");
+                    System.out.println("    ___________________________");
+                } else {
+                    System.out.println("    " + line);
+                    System.out.println("    ___________________________");
+                }
+            }
+        } while (!line.toLowerCase().contains("bye")); //Lines containing bye
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ___________________________");
+
+        in.close();
     }
 }
